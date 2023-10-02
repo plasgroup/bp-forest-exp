148052bbda5a59ced691d14b45cdb732c57676cb
okudahi <pabrika@mineo.jp>
2023-10-02 05:52:19 +0200

    add migration results

--- scripts/generate_workloads.sh
+++ scripts/generate_workloads.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p build
-for i in 0 0.5 0.99 1.2 1.5 2
+for i in 0 0.4 0.6 0.99 1.2
 do
 g++ ./ycsb-c/zipfian.cpp -DZIPFIAN_CONST=$i -o build/zipfian
 build/zipfian > workload/zipfianconst$i.csv
